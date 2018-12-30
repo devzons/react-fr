debdbb36de5d363ec4598d597ed39f00d39a8b8a
Don Yu <devzons@gmail.com>
2018-12-29 22:25:14 -0800

    Finish Todos

--- todoapp/src/components/TodoItem.js
+++ todoapp/src/components/TodoItem.js
@@ -3,9 +3,12 @@ import React from 'react'
 function TodoItem(props) {
   return (
     <div className="todo-list__item">
-      <input type="checkbox" checked={props.item.completed} onChange= {() => console.log("changed")} /> 
+      <input 
+        type="checkbox" 
+        checked={props.item.completed}
+        onChange= {() => props.handleChange(props.item.id)} 
+      /> 
       <p>{props.item.text}</p>
-      <p>{props.item.completed}</p>
     </div>
   )
 }
